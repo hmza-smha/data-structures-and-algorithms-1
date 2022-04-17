d2faa7880327474dbd1596069a1383ba555650ed
HamZa Akram Samha <smha.hmza@gmail.com>
2022-04-18 01:13:42 +0300

    stack-queue-animal-shelter CC12

---
+++ DSA-Testing/AnimalShilter_CC12.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using data_structures_and_algorithms_1.stack_queue_animal_shelter;
+using Xunit;
+
+namespace DSA_Testing
+{
+    public class AnimalShilter_CC12
+    {
+        [Fact]
+        public void Test_Dog()
+        {
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal dog1 = new Dog();
+            animalShelter.Enqueue(dog1);
+
+            Assert.True(animalShelter.Dogs.Dequeue() is Dog);
+        }
+
+
+        [Fact]
+        public void Test_Cat()
+        {
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal cat1 = new Cat();
+            animalShelter.Enqueue(cat1);
+
+            Assert.True(animalShelter.Cats.Dequeue() is Cat);
+        }
+
+
+        [Fact]
+        public void Test_Dog_Dequeue()
+        {
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal dog1 = new Dog();
+            animalShelter.Enqueue(dog1);
+
+            Assert.True(animalShelter.Dequeue("dog") is Dog);
+        }
+
+        [Fact]
+        public void Test_Cat_Dequeue()
+        {
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal cat1 = new Cat();
+            animalShelter.Enqueue(cat1);
+
+            Assert.True(animalShelter.Dequeue("cat") is Cat);
+        }
+    }
+}
---
+++ data-structures-and-algorithms-1/stack-queue-animal-shelter/Cat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms_1.stack_queue_animal_shelter
+{
+    public class Cat : Animal
+    {
+
+    }
+}
