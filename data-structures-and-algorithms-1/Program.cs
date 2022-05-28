be2c8a46839033639da7589b9987f036faf6e391
HamZa Akram Samha <smha.hmza@gmail.com>
2022-05-28 17:33:22 +0300

    MergeSort Doc

--- data-structures-and-algorithms-1/Program.cs
+++ data-structures-and-algorithms-1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using data_structures_and_algorithms_1;
 using data_structures_and_algorithms_1.binary_tree;
+using data_structures_and_algorithms_1.sorting.merge;
 using data_structures_and_algorithms_1.trees;
 
 namespace data_structures_and_algorithms
@@ -10,7 +11,9 @@ namespace data_structures_and_algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data Structures & Algorithms!");
+            //Console.WriteLine("Data Structures & Algorithms!");
+            int[] list = new int[] { 8, 4, 23, 42, 16, 15 };
+            MergeSort.Sort(list);
         }
     }
 }
