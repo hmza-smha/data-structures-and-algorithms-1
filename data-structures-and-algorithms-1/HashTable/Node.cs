c1ae8de607f3f79e0ee79856e1a959e3f4e9541f
HamZa Akram Samha <smha.hmza@gmail.com>
2022-06-06 22:37:13 +0300

    Hashtable

---
+++ data-structures-and-algorithms-1/HashTable/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms_1.HashTable
+{
+    public class Node
+    {
+        public int Key;
+        public string Value;
+        public Node Next;
+
+        public Node(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
