ebbabf0f3ea8ea4a0cefe42e74aabb4000404299
HamZa Akram Samha <smha.hmza@gmail.com>
2022-06-18 18:02:17 +0300

    hashmap-left-join

---
+++ data-structures-and-algorithms-1/HashTable/hashmap-left-join/Hashmap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace data_structures_and_algorithms_1.hashmap_left_join
+{
+    public class Hashmap
+    {
+        public static List<string> LeftJoin(Hashtable table1, Hashtable table2)
+        {
+
+            List<string> result = new List<string>();
+
+            foreach (DictionaryEntry pair in table1)
+            {
+                string row;
+
+                if (table2.ContainsKey(pair.Key))
+                {
+                    row = pair.Key + "," +  table1[pair.Key].ToString() + "," + table2[pair.Key].ToString();
+                }
+                else
+                {
+                    row = pair.Key + "," + table1[pair.Key].ToString() + ",null";
+                }
+
+                result.Add(row);
+            }
+
+            return result;
+        }
+    }
+}
--- data-structures-and-algorithms-1/Program.cs
+++ data-structures-and-algorithms-1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using data_structures_and_algorithms_1.hashmap_left_join;
 using data_structures_and_algorithms_1.HashTable.hashmap_repeated_word;
 
 namespace data_structures_and_algorithms
@@ -10,6 +11,32 @@ namespace data_structures_and_algorithms
         static void Main(string[] args)
         {
             //Console.WriteLine("Data Structures & Algorithms!");
+
+            Hashtable t1 = new Hashtable();
+            Hashtable t2 = new Hashtable();
+
+            t1.Add("diligent", "employed");
+            t1.Add("fond", "enamored");
+            t1.Add("guide", "usher");
+            t1.Add("outfit", "garb");
+            t1.Add("wrath", "anger");
+
+            t2.Add("diligent", "idle");
+            t2.Add("fond", "averse");
+            t2.Add("guide", "follow");
+            t2.Add("flow", "jam");
+            t2.Add("wrath", "delight");
+
+            foreach (var e in Hashmap.LeftJoin(t1, t2))
+            {
+                for (int i = 0; i < e.Length; i++)
+                {
+                    Console.Write(e[i]);
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
