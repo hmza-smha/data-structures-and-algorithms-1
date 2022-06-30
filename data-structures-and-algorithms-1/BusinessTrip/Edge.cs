b7993d3320670a2f0b96f95d4d0034b1c42f9cc3
HamZa Akram Samha <smha.hmza@gmail.com>
2022-06-30 20:46:55 +0300

    BusinessTrip Structure Added

---
+++ data-structures-and-algorithms-1/BusinessTrip/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms_1.BusinessTrip
+{
+    public class Edge
+    {
+        public Vertex From { get; set; }
+        public Vertex To { get; set; }
+        public int Weight { get; set; }
+
+        public Edge()
+        {
+            From = To = new Vertex();
+        }
+    }
+}
+
--- data-structures-and-algorithms-1/Program.cs
+++ data-structures-and-algorithms-1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using data_structures_and_algorithms_1.BusinessTrip;
+using System;
 
 namespace data_structures_and_algorithms
 {
@@ -6,7 +7,29 @@ namespace data_structures_and_algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data Structures & Algorithms!");
+            Graph graph = new Graph();
+
+            Vertex v1 = new Vertex("1");
+            Vertex v2 = new Vertex("2");
+            Vertex v3 = new Vertex("3");
+            Vertex v4 = new Vertex("4");
+            Vertex v5 = new Vertex("5");
+
+
+            graph.Add(v1);
+            graph.Add(v2);
+            graph.Add(v3);
+            graph.Add(v4);
+            graph.Add(v5);
+
+            graph.AddEdge(v1, v2, 0);
+
+            foreach (var e in graph.GetNeighbors(v1))
+            {
+                Console.WriteLine(e.Value);
+            }
+
+
         }
     }
 }
